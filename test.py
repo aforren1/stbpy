a5f5415609896d4c838d87e4eb566a8d17745bae
Alex Forrence <alex.forrence@gmail.com>
2020-06-24 11:03:16 -0400

    fix test

--- test.py
+++ test.py
@@ -1,18 +1,17 @@
 from stb import image as im
-import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 x = im.load('cat.png')
 y = im.resize(x, 200, 200)
 
-plt.imshow(x.reshape(x.shape[1], x.shape[0], x.shape[2]))
-plt.show()
+#plt.imshow(x.reshape(x.shape[1], x.shape[0], x.shape[2]))
+#plt.show()
 
-plt.imshow(y.reshape(y.shape[1], y.shape[0], y.shape[2]))
-plt.show()
+#plt.imshow(y.reshape(y.shape[1], y.shape[0], y.shape[2]))
+#plt.show()
 
 z = im.write_png_to_memory(x)
-print(np.prod(x.shape) - z.shape)
+print((x.shape[0] * x.shape[1] * x.shape[2]) - z.shape)
 
 w = im.load_from_memory(z)
 
