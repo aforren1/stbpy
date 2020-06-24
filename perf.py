bb53f334ef1985d123c55ef8d5ab0b4ec9cce86d
Alex Forrence <alex.forrence@gmail.com>
2020-06-24 15:27:43 -0400

    fix perf test

--- perf.py
+++ perf.py
@@ -1,19 +1,21 @@
 from timeit import timeit
+import os.path as op
 
+fn = op.join(op.dirname(__file__), "cat.png")
 def timethat(expr, number=1e5, setup='pass', globs=None):
     title = expr
     print('{:40} {:8.5f} µs'.format(title, timeit(expr, number=int(number), globals=globs, setup=setup) * 1000000.0 / float(number)))
 
 num = 5e2
 stb_setup = 'import stb.image as im'
-timethat('x = im.load("cat.png")', number=num, setup=stb_setup)
-
 pil_setup = 'from PIL import Image'
-timethat('x = Image.open("cat.png")', number=num, setup=pil_setup)
-timethat('x = Image.open("cat.png").getdata()', number=num, setup=pil_setup)
-timethat('x = Image.open("cat.png").tobytes()', number=num, setup=pil_setup)
-timethat('x = np.array(Image.open("cat.png"))', number=num, setup=pil_setup+';import numpy as np')
 
-timethat('y = im.resize(x, 200, 200)', number=num, setup=stb_setup + ';x=im.load("cat.png")')
-timethat('y = x.resize((200, 200))', number=num, setup=pil_setup + ';x=Image.open("cat.png")')
+timethat('x = im.load(fn)', number=num, setup=stb_setup, globs=globals())
+timethat('x = Image.open(fn)', number=num, setup=pil_setup, globs=globals())
+timethat('x = Image.open(fn).getdata()', number=num, setup=pil_setup, globs=globals())
+timethat('x = Image.open(fn).tobytes()', number=num, setup=pil_setup, globs=globals())
+timethat('x = np.array(Image.open(fn))', number=num, setup=pil_setup+';import numpy as np', globs=globals())
+
+timethat('y = im.resize(x, 200, 200)', number=num, setup=stb_setup + ';x=im.load(fn)', globs=globals())
+timethat('y = x.resize((200, 200))', number=num, setup=pil_setup + ';x=Image.open(fn)', globs=globals())
 
